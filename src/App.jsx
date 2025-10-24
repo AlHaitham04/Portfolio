c00aa19b0553d9548c0bbb72f26b40816c85d4a7
unknown <haitham8213@gmail.com>
2025-10-25 03:41:44 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { ProtectedRoute } from './ProtectedRoute';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <Routes>
         <Route path="/" element={<Account />} />
 
