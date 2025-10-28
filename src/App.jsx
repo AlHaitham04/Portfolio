5f993060db6216f89eaa26dd3ffcd663aedafbc9
unknown <haitham8213@gmail.com>
2025-10-29 02:28:44 +0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './Pages/Dashboard/dashboard';
 import { Stats } from './Pages/Stats/stats';
 import { Account } from './Pages/Account/account';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute';
 
 function App() {
   return (
-    <Router basename="/Portfolio">
+    <Router>
       <Routes>
         <Route path="/" element={<Account />} />
-
-        <Route
-          path="/Dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/Stats"
-          element={
-            <ProtectedRoute>
-              <Stats />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/Dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/Stats" element={<ProtectedRoute><Stats /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
